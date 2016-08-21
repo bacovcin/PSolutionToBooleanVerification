16ba48cce9f71307ea40c662a096e0a901b2be5c
Hezekiah Akiva Bacovcin <hbacovci@gmail.com>
2016-08-21 19:58:08 -0400

    Created correct version of satisfiability calculator

--- PSolutionToBooleanVerification.py
+++ PSolutionToBooleanVerification.py
@@ -7,71 +7,79 @@
 # the closing parenthesis. Example: (& A B (- C ) (- (| C D ) ) )
 
 
-class PropClause:
+class PropClause(object):
     def __init__(self, typeof, contents):
-        if typeof in ['&', '-']:
+        # Propositions can be conjunctions, negation or terminal propositions
+        # Check to make sure proper type was sent
+        if typeof in ['&', '-', 't']:
             self.typeof = typeof
         else:
             raise TypeError('Not a valid operator')
-        if typeof == '-' and len(contents) != 1:
-            print(contents)
-            print(len(contents))
-            raise TypeError('Provided non unary contents to unary operator')
+        # Assign contents to proposition
+        self.contents = frozenset(contents)
+        # Make sure content is list of propositions (possibly unary)
+        if False in [type(x) != '__main__.PropClause' for x in contents] and self.typeof != 't':
+            raise TypeError('Contents are not propositions')
         else:
-            self.contents = frozenset(contents)
-
-    def aSolution(self):
-        if self.isSatisfiable():
-            solution = {}
-            if self.typeof == '-':
-                lx = list(self.contents)
-                if type(lx[0]) is str:
-                    solution[lx[0]] = False
+            # Recursive check for satisfiability
+            # Three possible condtions:
+            #   1) Terminal proposition (return satisfiable)
+            #   2) Conjunction (check for contradictions)
+            #   3) Negation (depends on content):
+            #       a) If satisfiable, return satisfiable
+            #       b) If contradictory, return tautological
+            #       c) If tautological, return contradictory
+            # Possible responses are: Satisfiable ('s'), Contradictory ('c'), or
+            # Tautological ('t')
+            # At end:
+            #   Return True if not contradictory, otherwise return False
+            if self.typeof == 't':
+                self.satisfiable = 's'
+            elif self.typeof == '-':
+                if typeof == '-' and len(contents) != 1:
+                    # Negation is a unary operator
+                    raise TypeError('Provided non unary contents to unary operator')
+                elif contents[0].satisfiable == 's':
+                    self.satisfiable = 's'
+                elif contents[0].satisfiable == 'c':
+                    self.satisfiable = 't'
+                elif contents[0].satisfiable == 't':
+                    self.satisfiable = 'c'
+            else:  # self.typeof == '&'
+                # Contradictions arise when containing a negative conjunct
+                # whose contents are a subset of the non-negative content of
+                # the conjunction
+                satiscon = [x.satisfiable for x in contents]
+                if 'c' in satiscon: 
+                    # Conjunction of contradictions are contradictions
+                    self.satisfiable = 'c'
+                if 's' not in satiscon:
+                    # Conjunction of tautologies is a tautology
+                    self.satisfiable = 't'
                 else:
-                    neg_sol = lx[0].aSolution()
-                    for key in neg_sol.keys():
-                        solution[key] = not neg_sol[key]
-            else:
-                for x in list(self.contents):
-                    if type(x) is str:
-                        solution[x] = True
-                    else:
-                        lx = list(x.contents)
-                        if type(lx[0]) is str:
-                            solution[lx[0]] = False
-                        else:
-                            neg_sol = lx[0].aSolution()
-                            for key in neg_sol.keys():
-                                if key not in solution.keys():
-                                    solution[key] = not neg_sol[key]
-            return solution
-        else:
-            return False
-
-    def isSatisfiable(self):
-        if self.typeof == '-':
-            return True
-        for x in (self.contents):
-            try:
-                if x.typeof == '-':
-                    lx = list(x.contents)
-                    if type(lx[0]) is str:
-                        for y in list(self.contents):
-                            if y == lx[0]:
-                                return False
-                    else:
-                        for y in list(lx[0].contents):
-                            for z in list(self.contents):
-                                if z == y:
-                                    break
+                    # Collect all negative content and use that to test for
+                    # contradiction by checking if the content of each is a
+                    # subset of the content of the conjunction
+                    negatives = []
+                    for x in contents:
+                        if x.typeof == '-' and x.satisfiable != 't':
+                            if list(x.contents)[0].typeof == 't':
+                                negatives.append(set(x.contents))
                             else:
-                                break
-                        else:
-                            return False
-            except AttributeError:
-                continue
-        else:
-            return True
+                                negatives.append(set(list(x.contents)[0].contents))
+                    setcon = set([x for x in contents if x.satisfiable != 't'])
+                    for neg in negatives:
+                        if neg.issubset(setcon):
+                            # The contents of the negative conjunct is a subset
+                            # of the whole set, which is a contradiction (e.g.,
+                            # A & -A)
+                            self.satisfiable = 'c'
+                            break
+                    else:
+                        # No contradictions were found, so the conjunction must
+                        # be satisfiable
+                        self.satisfiable = 's'
+
 
     def __str__(self):
         return str('(' + self.typeof + ' ' +
@@ -116,7 +124,7 @@ def ParseFormula(x):
         # contents and reset the element tracker
         elif c == ' ':
             if element != '' and ' ' not in element:
-                contents.append(element)
+                contents.append(PropClause('t',[element]))
             element = ''
             i += 1
         # Current character is part of the name of an element, so add it to the
@@ -138,13 +146,13 @@ def ParseFormula(x):
     elif typeof == '|':
         newcontents = []
         for x in contents:
-            if type(x) is str:
+            if x.typeof == 't':
                 newcontents.append(PropClause('-', [x]))
             elif x.typeof == '-':
-                if type(list(x.contents)[0]) is str:
-                    newcontents.append(list(x.contents)[0])
+                if list(x.contents)[0].typeof == 't':
+                    newcontents += x.contents
                 else:
-                    newcontents += list(list(x.contents)[0].contents)
+                    newcontents += x.contents[0].contents
             else:
                 newcontents.append(PropClause('-', [x]))
         outClause = [PropClause('-', [PropClause('&', newcontents)])]
@@ -166,8 +174,7 @@ def ParseFormula(x):
 
 a = '(& A B (- C ) (- (| C D ) ) )'
 a = '(| A (- A ) )'
-# a = '(& (| x1 y1 ) (| x2 y2 ) (| x3 y3 ) (| x4 y4 ) (- x3 ) (- y3 ) )'
+a = '(& (| x1 y1 ) (| x2 y2 ) (| (- x3 ) (- y3 ) ) (| x4 y4 ) x3 (- y3 ) )'
 b = ParseFormula(a)
 print(b[0][0])
-print(b[0][0].isSatisfiable())
-print(b[0][0].aSolution())
+print(b[0][0].satisfiable)
